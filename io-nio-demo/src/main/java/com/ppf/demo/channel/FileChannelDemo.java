77f04186d368855193a83cd5703885b3800d7ef2
潘鹏飞 <49594857@qq.com>
2020-11-15 08:08:26 +0800

    file channel：读取数据

--- io-nio-demo/src/main/java/com/ppf/demo/channel/FileChannelDemo.java
+++ io-nio-demo/src/main/java/com/ppf/demo/channel/FileChannelDemo.java
@@ -1,6 +1,9 @@
 package com.ppf.demo.channel;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -9,8 +12,19 @@ import java.nio.channels.FileChannel;
  */
 public class FileChannelDemo {
     public static void main(String[] args) throws Exception{
+        String filePath = "/Users/panpengfei/Desktop/a.txt";
+        //write(filePath);
+        read(filePath);
+    }
+
+    /**
+     * 写入到文件
+     * @param path
+     * @throws IOException
+     */
+    static void write(String path) throws IOException {
         //1:打开输出流
-        FileOutputStream os = new FileOutputStream("/Users/panpengfei/Desktop/a.txt");
+        FileOutputStream os = new FileOutputStream(path);
 
         //2:获取file channel
         FileChannel channel = os.getChannel();
@@ -29,5 +43,30 @@ public class FileChannelDemo {
 
         //7:关闭通道
         os.close();
+    }
+
+    /**
+     * 从文件读取数据
+     * @param path
+     * @throws IOException
+     */
+    static void read(String path) throws IOException {
+        //0: 文件
+        File file = new File(path);
+
+        //1：打开输入流
+        FileInputStream is = new FileInputStream(file);
+
+        //2：获取file channel
+        FileChannel channel = is.getChannel();
+
+        //3：初始一个缓存区
+        ByteBuffer buffer = ByteBuffer.allocate((int) file.length());
+
+        //4：读取数据到缓存区
+        channel.read(buffer);
+
+        //5：打印
+        System.out.println(new String(buffer.array()));
     }
 }
